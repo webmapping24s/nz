e2ab10297b4e5dd25448ae301fee3bf6acbe5291
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-11 17:37:28 +0200

    Marker als GeoJSON eingebaut und Maßstab hinzugefügt

--- main.js
+++ main.js
@@ -12,15 +12,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([lat, lng]).addTo(map);
-marker.bindPopup(`
-    <h2>Tongariro Nationalpark</h2>
-    <ul>
-        <li>Breite: ${lat.toFixed(5)}</li>
-        <li>Länge: ${lng.toFixed(5)}</li>
-    </ul>
-`).openPopup();
-
 L.control.scale({
     imperial: false
+}).addTo(map);
+
+let jsonPunkt = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [lng, lat]
+    },
+    "properties": {
+        "name": "Tongariro Nationalpark"
+    }
+};
+
+L.geoJSON(jsonPunkt, {}).bindPopup(function (layer) {
+    return `
+    <h2>${layer.feature.properties.name}</h2>
+    <ul>
+        <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)}</li>
+        <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)}</li>
+    </ul>
+    `;
 }).addTo(map);
