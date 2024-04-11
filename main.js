df402b9beb7983867709f64cab6d170f859579f9
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-11 16:05:35 +0200

    Variablen lat und lng verwendet

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let map = L.map('map').setView([-39.2, 175.583333], 11);
+let lat = -39.2;
+let lng = 175.583333;
+
+let map = L.map('map').setView([lat, lng], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-39.2, 175.583333]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<h2>Hello world!</h2><br>I am a popup.").openPopup();
